f318fd800fc1cda60d373879d0efded980895cf9
Lucas Vocanson <lvocanson@gaming.tech>
2023-01-26 11:30:41 +0100

    Alterable stats
    + Init unit tests

---
+++ UnitTests/WindowTests.cs
@@ -0,0 +1,13 @@
+using BlueShadowMon;
+
+namespace UnitTests
+{
+    public class WindowTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
