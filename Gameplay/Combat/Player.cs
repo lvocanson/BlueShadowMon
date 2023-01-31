bbfb7577b7e792f62275d7195f068a33d94376f6
Jun <jzhi@gaming.tech>
2023-01-31 11:16:09 +0100

    Player and Inventory class
    Add player and inventory logical class
    Inventory Class definitely not finish yet
    
    (To be verified by Lucas San)

---
+++ Gameplay/Combat/Player.cs
@@ -0,0 +1,43 @@
+﻿
+namespace BlueShadowMon
+{
+    class Player
+    {
+        public Dictionary<Consumable, int> Inventory { get; private set; } = new();
+        public List<Pet> Pets { get; } = new();
+        
+
+        public Player(Dictionary<Consumable, int> inventory, List<Pet> pets)
+        {
+
+            
+        }
+
+        private void AddPet(Pet newPet)
+        {
+            Pets.Add(newPet);
+        }
+
+        private void AddConsumable(Consumable consumable, int amount)
+        {
+            Inventory.Add(consumable, amount);
+        }
+
+        private void RemovePet(Pet pet)
+        {
+            if (Pets.Contains(pet))
+            {
+                Pets.Remove(pet);
+            }
+        }
+
+        private void RemoveConsumable(Consumable consumable)
+        {
+            if (Inventory[consumable].Equals(0))
+            {
+                Inventory.Remove(consumable);
+            }
+        }
+
+    }
+}
---
+++ Graphics/Scenes/InventoryScene.cs
@@ -0,0 +1,35 @@
+﻿
+namespace BlueShadowMon
+{
+     class InventoryScene : Scene
+    {
+        public Inventory Inventory { get; private set; }
+
+        public InventoryScene(Inventory inventory)
+        {
+            Inventory = inventory;
+        }
+
+        public void Init(Inventory inventory)
+        {
+            Inventory = inventory;
+        }
+
+        public override void Draw()
+        {
+            int y = Console.WindowHeight - 3;
+            Window.Write(Inventory.CurrentMenu.Title, Window.MiddleX, y - 2, true);
+            for (int i = 0; i < Inventory.CurrentMenu.Length; i++)
+            {
+                Window.Write(Inventory.CurrentMenu[i], (int)(Console.WindowWidth * (i + 0.5) / Inventory.CurrentMenu.Length), y, true);
+            }
+        }
+
+        public override void KeyPressed(ConsoleKey key)
+        {
+            Inventory.KeyPressed(key);
+        }
+
+    }
+    
+}
